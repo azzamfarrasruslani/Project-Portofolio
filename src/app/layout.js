c2d5c55f797d4462c4e20cc9c49dd1cc9bddcaa3
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-03-27 12:26:00 +0700

    Add CustomCursor and Navbar components to RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,22 @@
-
 "use client";
+
+import "@/styles/cursor.css";
 import "@/styles/tailwind.css";
+import "@/lib/fontawesome";
+import CustomCursor from "@/app/components/CustomCursor";
+import Navbar from "@/app/components/Navbar";
 import { SessionProvider } from "next-auth/react";
 
 export default function RootLayout({ children }) {
   return (
     <SessionProvider>
       <html lang="en">
-        <body>{children}</body>
+        <head />
+        <body className="margin-0 p-0">
+          <CustomCursor />
+          <Navbar />
+          {children}
+        </body>
       </html>
     </SessionProvider>
   );
